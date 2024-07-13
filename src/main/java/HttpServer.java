195e096d68ad5455c11a2f1aa907add9ff1b0b8e
Minipasglop <dertest58@yahoo.fr>
2024-07-13 19:31:12 +0200

    fix(headers): too many crlf on headers

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -143,7 +143,7 @@ public class HttpServer {
         }
         String contentTypeHeader = "Content-Type: " + contentType + CRLF;
         String contentEncodingHeader = handleEncoding ? "Content-Encoding: " + SUPPORTED_ENCODING_OPTIONS + CRLF : "";
-        String contentLengthHeader = "Content-Length: " + contentLength + CRLF;
+        String contentLengthHeader = "Content-Length: " + contentLength;
         return contentTypeHeader + contentEncodingHeader + contentLengthHeader + CRLF;
     }
 
