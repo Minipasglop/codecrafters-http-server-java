0407f536b6b36e3a5a01343a2bfbf5b89004a762
Minipasglop <dertest58@yahoo.fr>
2024-07-13 16:00:54 +0200

    feature(path): handle 404 on unknown path

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,6 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -7,8 +9,6 @@ public class Main {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         System.out.println("Logs from your program will appear here!");
 
-        // Uncomment this block to pass the first stage
-
         try {
             ServerSocket serverSocket = new ServerSocket(4221);
 
@@ -16,7 +16,16 @@ public class Main {
             // ensures that we don't run into 'Address already in use' errors
             serverSocket.setReuseAddress(true);
             Socket clientSocket = serverSocket.accept(); // Wait for connection from client.
-            clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            String line = new BufferedReader(new InputStreamReader(clientSocket.getInputStream())).readLine();
+            String[] httpRequest = line.split(" ", 0);
+            // httpRequest[0] : HTTP Method
+            // httpRequest[1] : Path
+            // httpRequest[2] : HTTP Options
+            if (httpRequest[1].equals("/") || httpRequest[1].isBlank()) {
+                clientSocket.getOutputStream().write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+            } else {
+                clientSocket.getOutputStream().write("HTTP/1.1 404 Not Found\r\n\r\n".getBytes());
+            }
             System.out.println("accepted new connection");
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
