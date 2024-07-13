4daa82fc63a220c59c54dd44d8c95f04b65c48f5
Minipasglop <dertest58@yahoo.fr>
2024-07-13 20:08:26 +0200

    chore(refactor): Extract method business on smaller layers

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -87,38 +87,71 @@ public class HttpServer {
             if (requestPath.equals(BASE_PATH) || requestPath.isBlank()) {
                 httpResponse.setStatus(buildResponseStatus(STATUS_OK).getBytes(UTF_8));
             } else if (requestPath.startsWith(ECHO_PATH)) {
-                String textToEcho = requestPath.substring(requestPath.lastIndexOf(ECHO_PATH) + ECHO_PATH.length());
-                byte[] body = buildResponseBodyBytesHandlingEncoding(textToEcho, isEncodingValid);
-                httpResponse.setStatus(buildResponseStatus(STATUS_OK).getBytes(UTF_8));
-                httpResponse.setHeaders(buildResponseHeaders(CONTENT_TYPE_TEXT_PLAIN, isEncodingValid, body.length).getBytes(UTF_8));
-                httpResponse.setBody(body);
+                httpResponse = handleEchoCommand(requestPath, isEncodingValid);
             } else if (requestPath.equals(USER_AGENT_PATH)) {
-                String headerToPrintInBody = headers.stream().anyMatch(header -> header.startsWith(USER_AGENT_HEADER_PREFIX)) ? headers.stream().filter(header -> header.startsWith(USER_AGENT_HEADER_PREFIX)).findAny().get().substring(USER_AGENT_HEADER_PREFIX.length()) : "";
-                httpResponse.setStatus(buildResponseStatus(STATUS_OK).getBytes(UTF_8));
-                httpResponse.setHeaders(buildResponseHeaders(CONTENT_TYPE_TEXT_PLAIN, isEncodingValid, headerToPrintInBody.length()).getBytes(UTF_8));
-                httpResponse.setBody(buildResponseBody(headerToPrintInBody).getBytes(UTF_8));
+                httpResponse = handleUserAgentCommand(headers, isEncodingValid);
             } else if (requestPath.startsWith(FILE_PATH)) {
-                String filePath = requestPath.substring(requestPath.lastIndexOf(FILE_PATH) + FILE_PATH.length());
-                if (requestMethod.equals(GET_METHOD)) {
-                    String fileContent = new String(Files.readAllBytes(Paths.get(basePath + filePath)));
-                    if (!fileContent.isBlank()) {
-                        httpResponse.setStatus(buildResponseStatus(STATUS_OK).getBytes(UTF_8));
-                        httpResponse.setHeaders(buildResponseHeaders(CONTENT_TYPE_OCTET_STREAM, isEncodingValid, fileContent.length()).getBytes(UTF_8));
-                        httpResponse.setBody(buildResponseBody(fileContent).getBytes(UTF_8));
-                    }
-                } else if (requestMethod.equals(POST_METHOD)) {
-                    BufferedWriter writer = new BufferedWriter(new FileWriter(basePath + filePath));
-                    writer.write(payload.toString());
-                    writer.close();
-                    httpResponse.setStatus(buildResponseStatus(STATUS_OK_CREATED).getBytes(UTF_8));
-                }
+                httpResponse = handleFileCommand(basePath, requestPath, requestMethod,  isEncodingValid, payload);
             }
         } catch (Exception e) {
             System.out.println(Arrays.toString(e.getStackTrace()));
+            httpResponse = handleException(e);
         } finally {
             clientSocket.getOutputStream().write(httpResponse.getStatus());
             clientSocket.getOutputStream().write(httpResponse.getHeaders());
             clientSocket.getOutputStream().write(httpResponse.getBody());
         }
+    }
+
+    private HttpResponse handleException(Exception e) {
+        return HttpResponse.builder()
+                .status(buildResponseStatus(STATUS_INTERNAL_ERROR).getBytes(UTF_8))
+                .headers(buildResponseHeaders(CONTENT_TYPE_TEXT_PLAIN, false, e.getMessage().length()).getBytes(UTF_8))
+                .body(e.getMessage().getBytes(UTF_8))
+                .build();
+    }
+
+    private HttpResponse handleEchoCommand(String requestPath, boolean isEncodingValid) {
+        String textToEcho = requestPath.substring(requestPath.lastIndexOf(ECHO_PATH) + ECHO_PATH.length());
+        byte[] body = buildResponseBodyBytesHandlingEncoding(textToEcho, isEncodingValid);
+        return HttpResponse.builder()
+                .status(buildResponseStatus(STATUS_OK).getBytes(UTF_8))
+                .headers(buildResponseHeaders(CONTENT_TYPE_TEXT_PLAIN, isEncodingValid, body.length).getBytes(UTF_8))
+                .body(body)
+                .build();
+    }
+
+    private HttpResponse handleUserAgentCommand(List<String> headers, boolean isEncodingValid) {
+        String headerToPrintInBody = headers.stream().anyMatch(header -> header.startsWith(USER_AGENT_HEADER_PREFIX)) ? headers.stream().filter(header -> header.startsWith(USER_AGENT_HEADER_PREFIX)).findAny().get().substring(USER_AGENT_HEADER_PREFIX.length()) : "";
+        return HttpResponse.builder()
+                .status(buildResponseStatus(STATUS_OK).getBytes(UTF_8))
+                .headers(buildResponseHeaders(CONTENT_TYPE_TEXT_PLAIN, isEncodingValid, headerToPrintInBody.length()).getBytes(UTF_8))
+                .body(buildResponseBody(headerToPrintInBody).getBytes(UTF_8))
+                .build();
+    }
+
+    private HttpResponse handleFileCommand(String basePath, String requestPath, String requestMethod, boolean isEncodingValid, StringBuilder payload) throws IOException {
+        HttpResponse httpResponse = null;
+        String filePath = requestPath.substring(requestPath.lastIndexOf(FILE_PATH) + FILE_PATH.length());
+        if (requestMethod.equals(GET_METHOD)) {
+            String fileContent = new String(Files.readAllBytes(Paths.get(basePath + filePath)));
+            if (!fileContent.isBlank()) {
+                httpResponse = HttpResponse.builder()
+                        .status(buildResponseStatus(STATUS_OK).getBytes(UTF_8))
+                        .headers(buildResponseHeaders(CONTENT_TYPE_OCTET_STREAM, isEncodingValid, fileContent.length()).getBytes(UTF_8))
+                        .body(buildResponseBody(fileContent).getBytes(UTF_8))
+                        .build();
+            }
+        } else if (requestMethod.equals(POST_METHOD)) {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(basePath + filePath));
+            writer.write(payload.toString());
+            writer.close();
+            httpResponse = HttpResponse.builder()
+                    .status(buildResponseStatus(STATUS_OK_CREATED).getBytes(UTF_8))
+                    .headers(buildEmptyResponseHeaders().getBytes(UTF_8))
+                    .body(buildEmptyResponseBody().getBytes(UTF_8))
+                    .build();
+        }
+        return httpResponse;
     }
 }
--- src/main/java/constants/HttpConstants.java
+++ src/main/java/constants/HttpConstants.java
@@ -6,6 +6,7 @@ public class HttpConstants {
     public static final String STATUS_OK = "200 OK";
     public static final String STATUS_OK_CREATED = "201 Created";
     public static final String STATUS_NOT_FOUND = "404 Not Found";
+    public static final String STATUS_INTERNAL_ERROR = "500 Internal Error";
     public static final String HTTP_PROTOCOL_VERSION = "HTTP/1.1";
     public static final String CONTENT_TYPE_TEXT_PLAIN = "text/plain";
     public static final String CONTENT_TYPE_OCTET_STREAM = "application/octet-stream";
